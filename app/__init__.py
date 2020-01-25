2d97dc01e91a785871fb977ddbf392d8a502a540
Pulido V. P. E <pulidovpe.dev@gmail.com>
2020-01-24 20:12:24 -0400

    Adding flask_bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap()
 
 from .views import page
 
 def create_app(config):
 	app.config.from_object(config)	## Reiniciar servidor
+	bootstrap.init_app(app)
 	app.register_blueprint(page)
 	return app
