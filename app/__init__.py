e810e2eaf5d52c9e73b3dab186e81978f9636cb3
Pulido V. P. E <pulidovpe.dev@gmail.com>
2020-01-31 16:08:37 -0400

    CSRF

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 bootstrap = Bootstrap()
+csrf = CSRFProtect()
 
 from .views import page
 
 def create_app(config):
 	app.config.from_object(config)	## Reiniciar servidor
+
+	csrf.init_app(app)
 	bootstrap.init_app(app)
+	
 	app.register_blueprint(page)
 	return app
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-	pass
+	SECRET_KEY = 'llavesecreta'
 
 class DevelopmentConfig(Config):
 	DEBUG = True
