5783f4d4ffeb3c604c0cb0ff171598eab25546a0
Pulido V. P. E <pulidovpe.dev@gmail.com>
2020-02-01 13:48:36 -0400

    Encriptacion de password

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 import datetime
 
+from werkzeug.security import generate_password_hash
+
 from . import db
 
 class User(db.Model):
@@ -7,9 +9,18 @@ class User(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(50), unique=True, nullable=False)
-	password = db.Column(db.String(93), nullable=False)
+	encrypted_password = db.Column(db.String(128), nullable=False)
 	email = db.Column(db.String(100), unique=True, nullable=False)
 	created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+
+	@property
+	def password(self):
+		pass
+
+	@password.setter
+	def password(self, value):
+		self.encrypted_password = generate_password_hash(value)
+	
 
 	def __str__(self):
 		return self.username
