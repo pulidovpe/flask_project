3cd10f12435451a3fc6a85931434cebcdee8628a
Pulido V. P. E <pulidovpe.dev@gmail.com>
2020-02-02 16:36:44 -0400

    Mensaje por categoria y cierre de sesion

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
-from flask import render_template, request, flash
+from flask import render_template, request, flash, redirect, url_for
 
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 from .models import User
 from .forms import LoginForm, RegisterForm
@@ -35,11 +35,18 @@ def login():
 				login_user(user)
 				flash("Usuario autenticado exitosamente.")
 			else:
-				flash("Password incorrecto.")
+				flash("Password incorrecto.", 'error')
 		else:
-			flash("Usuario no existe.")
+			flash("Usuario no existe.", 'error')
 
 	return render_template('auth/login.html', title='Login', form=form)
+
+@page.route('/logout')
+def logout():
+	logout_user()
+	flash("Sesión cerrada exitosamente!")
+	return redirect(url_for('.login'))
+
 
 @page.route('/register', methods=['GET', 'POST'])
 def register():
@@ -48,7 +55,7 @@ def register():
 	if request.method == 'POST':
 		if form.validate():
 			user = User.create_element(form.username.data, form.password.data, form.email.data)
-			flash("Usuario registrado exitosamente")
+			flash("Usuario registrado exitosamente!")
 			##print(user.id)
 
 	return render_template('auth/register.html', title='Registro', form=form)
