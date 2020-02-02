c30627b2553ec55a144ba9d070e0bebb95e7ec60
Pulido V. P. E <pulidovpe.dev@gmail.com>
2020-02-02 17:36:26 -0400

    Se agrego modelo Task

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
 class DevelopmentConfig(Config):
 	DEBUG = True 	## Reiniciar servidor
 	SQLALCHEMY_DATABASE_URI = 'mysql://developer:123456@localhost/bd_project_web_facilito'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
 	'development': DevelopmentConfig,
